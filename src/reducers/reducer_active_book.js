62e77e53bdf0d24f70e675286bd5a6e3dee57ba0
joshgressman <joshgressman@gmail.com>
2018-07-02 19:10:58 -0500

    added new reducer

---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,6 @@
+
+//State argument is not application state, only the state this reducer is responsible for
+//Action is triggered and state is manipulated
+export default function(state, action) {
+
+}
